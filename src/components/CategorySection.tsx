24a0eedac5853a550c98beac3deb9530a7d00ebc
Bhargav Patel <A00303747@canadahanson.ca>
2025-07-03 18:37:01 -0400

    Improve the code

--- src/components/CategorySection.tsx
+++ src/components/CategorySection.tsx
@@ -6,20 +6,20 @@ const CategorySection = () => {
     return (
         <div className='p-5 mt-2'>
             <div>
-                <h2 id='movies' className='text-2xl text-white'>Movies</h2>
+                <h2 id='movies' className='text-4xl font-semibold text-white'>Movies</h2>
                 <div className='flex w-full gap-4 scroll-smooth overflow-x-auto pt-5 pb-5 px-3'>
                     {movieImages.map((element, index) => (
                         <ImageSection key={index} image={element.imageURL} />
                     ))}
                 </div>
-                <h2 id='series' className='text-2xl text-white'>Series</h2>
+                <h2 id='series' className='text-4xl font-semibold text-white'>Series</h2>
                 <div className='flex w-full gap-4 scroll-smooth overflow-x-auto pt-5 pb-5 px-3'>
                     {seriesImages.map((element, index) => (
                         <ImageSection key={index} image={element.imageURL} />
                     ))}
                 </div>
-                <h2 id='cartoon' className='text-2xl text-white'>Cartoon</h2>
-                <div className='flex w-full gap-5 scroll-smooth overflow-x-auto pt-4 pb-5 px-3'>
+                <h2 id='cartoon' className='text-4xl font-semibold text-white'>Cartoon</h2>
+                <div className='flex w-full gap-5 scroll-smooth overflow-x-auto whitespace-nowrap pt-4 pb-5 px-3'>
                     {cartoonImages.map((element, index) => (
                         <ImageSection key={index} image={element.imageURL} />
                     ))}
